8a4689ed25d7f2bcb058beaff610dd5187d9e804
Vls <bestblodd@gmail.com>
2021-09-01 19:16:54 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import './assets/css/normalize.scss';
+import './assets/css/main.scss';
+
+
+//BLOCKS 
+
+import './assets/css/blocks/btn.scss';
+import './assets/css/blocks/mobile-menu.scss';
+import './assets/css/blocks/popup.scss';
+import './assets/css/blocks/header.scss';
+import './assets/css/blocks/inputs.scss';
+import './assets/css/blocks/aside.scss';
+import './assets/css/blocks/footer.scss';
+import './assets/css/blocks/main-screen.scss';
+import './assets/css/blocks/main-catalog.scss';
+import './assets/css/blocks/breadcrumb.scss';
+import './assets/css/blocks/travel-cards.scss';
+import './assets/css/blocks/socials.scss';
+import './assets/css/blocks/toogles.scss';
+import './assets/css/blocks/main-product.scss';
+import './assets/css/blocks/block-info.scss';
+import './assets/css/blocks/test-info.scss';
+import './assets/css/blocks/steps.scss';
+import './assets/css/blocks/sidebar.scss';
+import './assets/css/blocks/advantages.scss';
+import './assets/css/blocks/you-contact.scss';
+import './assets/css/blocks/director.scss';
+import './assets/css/blocks/aspiration.scss';
+import './assets/css/blocks/table.scss';
+import './assets/css/blocks/booking-success.scss';
+
+
+
+
+
+import './assets/css/blocks/pack.scss';
+import './assets/css/blocks/content.scss';
+
+// INDEX
+import './assets/css/blocks/main-screen.scss';
+import './assets/css/blocks/travel-packs.scss';
+import './assets/css/blocks/about.scss';
+import './assets/css/blocks/certification.scss';
+import './assets/css/blocks/business.scss';
+import './assets/css/blocks/contact.scss';
+import './assets/css/blocks/confidence.scss';
+import './assets/css/blocks/faq.scss';
+import './assets/css/blocks/block-travel.scss';
+
+
+
+
+// JS
+
+import './assets/js/fixed';
+import './assets/js/toogles';
+import './assets/js/popup';
+import './assets/js/inputs';
+import './assets/js/inputs-mask';
+import './assets/js/header';
+import './assets/js/mobile-menu';
+// import './assets/js/anchors';
+// import './assets/js/mobile-menu';
+import './assets/js/anchors';
